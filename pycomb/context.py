b9d6bceb08e81b5373d8e102e4103f7085df6b8e
mirko <fcracker79@gmail.com>
2016-11-06 21:22:45 +0100

    Names refactory. Initial production mode support.

--- pycomb/context.py
+++ pycomb/context.py
@@ -4,17 +4,17 @@ import abc
 class ValidationErrorObserver(metaclass=abc.ABCMeta):
     @abc.abstractmethod
     def on_error(self, ctx, expected_type, found_type):
-        pass
+        pass  # pragma: no cover
 
 
 class ValidationErrorObservable(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def add_error_listener(self, error_listener):
-        pass
+    def add_error_observer(self, error_observer):
+        pass  # pragma: no cover
 
     @abc.abstractmethod
     def notify_error(self, expected_type, found_type):
-        pass
+        pass  # pragma: no cover
 
 
 class ValidationContext(ValidationErrorObservable, metaclass=abc.ABCMeta):
@@ -31,12 +31,18 @@ class ValidationContext(ValidationErrorObservable, metaclass=abc.ABCMeta):
     def empty(self):
         pass  # pragma: no cover
 
+    @property
+    @abc.abstractmethod
+    def production_mode(self):
+        pass  # pragma: no cover
+
 
 class ValidationContextImpl(ValidationContext):
-    def __init__(self):
+    def __init__(self, production_mode):
         self._path = []
         self._path_str = None
-        self._error_listeners = []
+        self._error_observers = []
+        self._production_mode = production_mode
 
     def append(self, path_element, separator='.'):
         self._path_str = None
@@ -55,18 +61,22 @@ class ValidationContextImpl(ValidationContext):
         return not bool(self._path)
 
     def copy(self):
-        result = ValidationContextImpl()
+        result = ValidationContextImpl(self.production_mode)
         result._path = [x for x in self._path]
         result._path_str = self._path_str
-        result._error_listeners = [x for x in self._error_listeners]
+        result._error_observers = [x for x in self._error_observers]
         return result
 
-    def add_error_listener(self, error_listener):
-        self._error_listeners.append(error_listener)
+    def add_error_observer(self, error_observer):
+        self._error_observers.append(error_observer)
 
     def notify_error(self, expected_type, found_type):
-        for l in self._error_listeners:
+        for l in self._error_observers:
             l.on_error(self, expected_type, found_type)
+
+    @property
+    def production_mode(self):
+        return self._production_mode
 
 
 def _assert_msg(guard, msg, ctx):
@@ -88,9 +98,9 @@ class _DefaultValidationErrorObserver(ValidationErrorObserver):
 _default_validation_error_observer = _DefaultValidationErrorObserver()
 
 
-def create(base_ctx=None, validation_error_observer=_default_validation_error_observer):
-    result = base_ctx.copy() if base_ctx else ValidationContextImpl()
+def create(base_ctx=None, validation_error_observer=_default_validation_error_observer,
+           production_mode=False):
+    result = base_ctx.copy() if base_ctx else ValidationContextImpl(production_mode)
     if validation_error_observer:
-        result.add_error_listener(validation_error_observer)
+        result.add_error_observer(validation_error_observer)
     return result
-
