a36a6d182b720ce904e16e8c38739f3cf7265e96
mirko <fcracker79@gmail.com>
2016-11-13 23:38:49 +0100

    Attempt to make Pypi work

--- README.txt
+++ README.txt
@@ -1,5 +1,3 @@
-[![Build Status](https://travis-ci.org/fcracker79/pycomb.svg?branch=master)](https://travis-ci.org/fcracker79/pycomb)
-
 PyComb
 ======
 
--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_packages(path):
 
 packages = get_packages('pycomb')
 setup(name='pycomb',
-      version='0.1.0',
+      version='0.1.1',
       description='Python combination',
       url='https://github.com/fcracker79/pycomb',
       author='fcracker79',
