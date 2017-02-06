8ee10c10c3e5606fc1ede2e830c67dd514ab9ac2
mirko <fcracker79@gmail.com>
2017-02-06 19:34:42 +0100

    New release: 0.1.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_packages(path):
 
 packages = get_packages('pycomb')
 setup(name='pycomb',
-      version='0.1.7',
+      version='0.1.8',
       description='Python combination',
       url='https://github.com/fcracker79/pycomb',
       author='fcracker79',
