6d546886fac9d49cf08848366a6b4d388a3c6378
fcracker79 <fcracker79@gmail.com>
2019-06-08 14:58:49 +0200

    New release 0.1.17

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def get_packages(path):
 
 packages = get_packages('pycomb')
 setup(name='pycomb',
-      version='0.1.16',
+      version='0.1.17',
       description='Python combination',
       long_description=readfile('README.rst'),
       url='https://github.com/fcracker79/pycomb',
